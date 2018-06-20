089788b4a84f8d793f9865b0daf56a7080f67e13
ithallojunior <ithallojunior@outlook.com>
2018-06-20 00:50:21 -0300

    adding more stuff to the botw

--- ga_mlp_bot/bot.py
+++ ga_mlp_bot/bot.py
@@ -1,4 +1,4 @@
-#-*-utf-8-*-
+# -*- coding: utf-8 -*-
 import telepot
 import time
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
@@ -12,27 +12,55 @@ class health_bot():
     
     def __init__(self,):
         self.sleep_time = 1.
-        self.state = 0 
+        self.state = "init"
         self.block_counter = 0
         self.max_to_block = 4 # plus 1
         self.busy_flag = False #allows just one user at a time
         self.user = USER
         self.bot  = telepot.Bot(TOKEN)
-        self.lang = "en_us"
+        
+
+        #dictionaries, will make it easier to translate
+        self.names2states = {
+            "init":"init",
+            "Set Population":"set_pop",
+            "Total Generations":"set_gen",
+            "Crossover Ratio":"set_cros",
+            "About":"about",
+            "Choose Parameters":"cho_par",
+            "Exit":"exit",
+            "Run>>":"run"}
+            
+        self.states2names = self._invert_dict(self.names2states)
+
+        #keyboards
+        self.default_keyboard = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text="%s"%self.states2names["cho_par"])],
+        [KeyboardButton(text="%s"%self.states2names["exit"]),
+        KeyboardButton(text="%s"%self.states2names["about"])],
+        [KeyboardButton(text="%s"%self.states2names["run"])] ],
+        resize_keyboard=True,
+        one_time_keyboard=False)
+    
+    # dictionary inverter 
+    def _invert_dict(self, d):
+        return dict([ (v, k) for k, v in d.iteritems( ) ])
     
     #locker, if not found or recognized
     def _lock(self, chat_id, msg):
        # bypass for the programmer 
         if (msg["from"]["username"] == self.user) or (msg["from"]["username"] == USER):
             self.busy_flag = True
+            print "user access: %s"%msg["from"]["username"]
             return False 
-        elif(msg["from"]["username"]!= self.user) and (not self.busy_flag) :
+        elif(msg["from"]["username"]!= self.user) and (not self.busy_flag):
             if self.block_counter < self.max_to_block:
-
+                print "user trying to access: %s "%msg["from"]["username"]
                 if self.block_counter == 0:
                     self.bot.sendMessage(chat_id, "Type the password:") 
                 
                 if msg["text"]==PASSWORD:
+                    print "new user from password: %s "%msg["from"]["username"]
                     self.user = msg["from"]["username"]
                     self.busy_flag = True
                     self.bot.sendMessage(chat_id, "Password Accepted!") 
@@ -43,22 +71,62 @@ class health_bot():
                         self.bot.sendMessage(chat_id, "Wrong password! Type again!") 
                     self.block_counter += 1    
             else:
+                print "user blocked trying to access: %s"%msg["from"]["username"]
                 self.bot.sendMessage(chat_id, "Bot blocked for you!") 
         else:
-           self.bot.sendMessage(chat_id, "Bot busy")
+            self.bot.sendMessage(chat_id, "Bot busy")
+            print "Bot busy:  %s"%msg["from"]["username"]
         return True
    
+
+    #defines the next state
+    def next_state(self, msg):
+        try:
+            self.state = self.names2states[msg["text"]]
+            print "Next state:", self.state
+            
+        except KeyError:
+            print "Not a key:", msg["text"]
+            print "State", self.state
+        
+        
+
     #chat manager
     def _chat_handler(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
                     
         #asking password and or checking user
         if (not self._lock(chat_id, msg)): 
-                        
-            self.bot.sendMessage(chat_id, "HELLO, %s"%self.user)
-            # moving to next state
-            self.state += 1
-            print "State", self.state
+            
+            self.next_state(msg)
+            if self.state == "init":   
+                self.bot.sendMessage(chat_id, "HELLO, %s"%self.user,
+                    reply_markup=self.default_keyboard)
+
+            elif self.state == "set_pop":
+                pass
+
+            elif self.state == "set_gen":
+                pass
+            
+            elif self.state == "set_cros":
+                pass
+            elif self.state == "about":
+                self.bot.sendMessage(chat_id, "--->Bot developed by:")
+                self.bot.sendMessage(chat_id, "Ithallo J.A.G.🍺,\nJosé E.S.,\nRenata M.L.,\nRoberta M.L.C.")
+                self.bot.sendMessage(chat_id, "For educational purposes only")
+            
+            elif self.state == "exit":
+                #reseting
+                self.state = "init"
+                self.block_counter = 0
+                self.max_to_block = 4 # plus 1
+                self.busy_flag = False #allows just one user at a time
+                self.user = USER
+                self.bot.sendMessage(chat_id, "Bye!👋🏼")
+            else:
+                pass        
+
     #callbacks, if I use it
     def _on_callback(self, msg):
         pass
